44a55efcd0f185fe84cf74471ee0078b9bb1337e
Yunus-Anwar <yunnaboss94@gmail.com>
2024-11-11 13:50:26 +0300

    Commit
    My portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings: ["Full Stack Developer", "Web Designer", "Mobile App Developer", "AI enthusiast"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
